6fee7f3170cc3c17fc9853749e94e9961a0c55c5
mattrdunn <mardunn@ucsc.edu>
2018-04-30 15:47:26 -0700

    Linked sign in page to sign up page, still need to add Firebase functionality to everything.

--- app/src/main/java/cmps121/matt/housemates/SignIn.java
+++ app/src/main/java/cmps121/matt/housemates/SignIn.java
@@ -1,7 +1,10 @@
 package cmps121.matt.housemates;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        final Button signUpButton = findViewById(R.id.sign_up_button);
+        signUpButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignIn.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 
     //hello
